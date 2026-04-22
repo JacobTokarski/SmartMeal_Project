694787bde3370bcb04a05d9563deda70a36b5292
Jakub <ktokarski2003@o2.pl>
2026-04-22 14:08:18 +0200

    Initialized Voyager navigation

--- composeApp/src/commonMain/kotlin/org/example/smartmeal/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/smartmeal/App.kt
@@ -14,6 +14,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
+import org.example.smartmeal.ui.views.login.LoginScreen
 import org.jetbrains.compose.resources.painterResource
 
 import smartmeal_project.composeapp.generated.resources.Res
@@ -23,27 +26,8 @@ import smartmeal_project.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        Navigator(LoginScreen()) { navigator ->
+            SlideTransition(navigator)
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/login/LoginScreen.kt
@@ -0,0 +1,27 @@
+package org.example.smartmeal.ui.views.login
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+
+class LoginScreen: Screen {
+    @Composable
+    override fun Content() {
+        LoginContent()
+    }
+}
+
+@Composable
+fun LoginContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("SmartMeal - Login Screen!")
+    }
+}

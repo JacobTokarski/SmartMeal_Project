9a8db8b0074d5c980e70c0e983ff1ebc910c57f8
Jakub <ktokarski2003@o2.pl>
2026-04-23 12:29:37 +0200

    Added authentication assets and resources

--- composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/login/LoginScreen.kt
@@ -1,12 +1,20 @@
 package org.example.smartmeal.ui.views.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import org.jetbrains.compose.resources.painterResource
+import smartmeal_project.composeapp.generated.resources.Res
+
 
 class LoginScreen: Screen {
     @Composable

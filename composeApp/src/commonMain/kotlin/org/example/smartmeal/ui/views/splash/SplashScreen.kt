5b6e716cc2f432fdf870dae17f2f3699432793d0
Jakub <ktokarski2003@o2.pl>
2026-05-06 15:42:47 +0200

    Modified SplashScreen v2

--- composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/splash/SplashScreen.kt
@@ -1,10 +1,14 @@
 package org.example.smartmeal.ui.views.splash
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -43,41 +47,49 @@ object SplashScreen : Screen {
 fun SplashContent() {
     Surface(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .systemBarsPadding(),
         color = Color.White
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize(),
         ) {
-            Image(
-                painter = painterResource(Res.drawable.pic_leaf),
-                contentDescription = null,
+            Box(
                 modifier = Modifier
-                    .width(120.dp)
-                    .height(280.dp)
-                    .offset(((-15).dp), y = ((-20).dp))
-            )
+                    .fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(Res.drawable.pic_leaf),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(120.dp)
+                        .height(280.dp)
+                        .align(Alignment.TopStart)
+                        .offset(((-10).dp), y = ((-10).dp))
+                )
 
 
-            Image(
-                painter = painterResource(Res.drawable.pic_logo),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(300.dp)
-                    .width(300.dp)
-                    .align(Alignment.CenterHorizontally)
-            )
+                Image(
+                    painter = painterResource(Res.drawable.pic_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(300.dp)
+                        .width(300.dp)
+                        .align(Alignment.Center)
+                )
 
-            Image(
-                painter = painterResource(Res.drawable.pic_range),
-                contentDescription = null,
-                modifier = Modifier
-                    .width(128.dp)
-                    .height(134.dp)
-                    .align(Alignment.End)
-                    .offset(x = 10.dp, y = 200.dp)
-            )
+                Image(
+                    painter = painterResource(Res.drawable.pic_range),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(128.dp)
+                        .height(134.dp)
+                        .align(Alignment.BottomEnd)
+                        .navigationBarsPadding()
+                        .offset(x = 10.dp, y = 0.dp)
+                )
+            }
         }
     }
 }

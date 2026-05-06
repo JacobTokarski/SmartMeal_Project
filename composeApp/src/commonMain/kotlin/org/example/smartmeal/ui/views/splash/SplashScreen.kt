29223c21c2cde9cb58de3f3e281651fd5ad23160
Jakub <ktokarski2003@o2.pl>
2026-05-06 15:27:39 +0200

    Modified SplashScreen

--- composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/splash/SplashScreen.kt
@@ -1,10 +1,10 @@
 package org.example.smartmeal.ui.views.splash
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -20,9 +20,11 @@ import kotlinx.coroutines.delay
 import org.example.smartmeal.ui.views.login.LoginScreen
 import org.jetbrains.compose.resources.painterResource
 import smartmeal_project.composeapp.generated.resources.Res
+import smartmeal_project.composeapp.generated.resources.pic_leaf
 import smartmeal_project.composeapp.generated.resources.pic_logo
+import smartmeal_project.composeapp.generated.resources.pic_range
 
-object SplashScreen: Screen {
+object SplashScreen : Screen {
     @Composable
     override fun Content() {
 
@@ -47,17 +49,35 @@ fun SplashContent() {
         Column(
             modifier = Modifier
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ) {
+            Image(
+                painter = painterResource(Res.drawable.pic_leaf),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(280.dp)
+                    .offset(((-15).dp), y = ((-20).dp))
+            )
+
+
             Image(
                 painter = painterResource(Res.drawable.pic_logo),
                 contentDescription = null,
                 modifier = Modifier
                     .height(300.dp)
                     .width(300.dp)
+                    .align(Alignment.CenterHorizontally)
             )
 
+            Image(
+                painter = painterResource(Res.drawable.pic_range),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(128.dp)
+                    .height(134.dp)
+                    .align(Alignment.End)
+                    .offset(x = 10.dp, y = 200.dp)
+            )
         }
     }
 }

f2c1777b58bba727326f9f81c95312231f3da64f
Jakub <ktokarski2003@o2.pl>
2026-05-10 12:08:15 +0200

    Created CustomRecipeCard component, various changes in OwnScreen and OwnViewModel

--- composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/own/OwnScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/own/OwnScreen.kt
@@ -1,9 +1,16 @@
 package org.example.smartmeal.ui.views.own
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,14 +21,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
-import org.example.smartmeal.ui.components.CustomSearchField
+import org.example.smartmeal.ui.components.CustomRecipeCard
 import org.example.smartmeal.ui.views.own.parts.OwnEmptyState
 import org.example.smartmeal.ui.views.own.parts.OwnFooter
 import org.example.smartmeal.ui.views.own.parts.OwnHeader
 import org.koin.compose.viewmodel.koinViewModel
-import smartmeal_project.composeapp.generated.resources.Res
 
-object OwnScreen: Screen {
+object OwnScreen : Screen {
     @Composable
     override fun Content() {
         val viewModel = koinViewModel<OwnViewModel>()
@@ -54,28 +60,56 @@ fun OwnContent(
 
             OwnHeader(
                 query = state.searchQuery,
-                onQueryChange = { viewModel.onSearchQuery(it)}
+                onQueryChange = { viewModel.onSearchQuery(it) }
             )
+
+            Spacer(modifier = Modifier.height(10.dp))
 
             Box(
                 modifier = Modifier
                     .weight(1f),
                 contentAlignment = Alignment.Center
             ) {
-                if (state.recipes.isEmpty()) {
-                    OwnEmptyState()
-                } else {
+                when {
 
-                    // Logika LazyColumn w przyszłość i kreowanie formularza
-                    Text(
-                        text = "W przyszłości formularz!"
-                    )
+                    state.isFormVisible -> {
+                        Text(
+                            text = "Description for now"
+                        )
+                    }
+
+                    state.recipes.isEmpty() -> {
+                        OwnEmptyState()
+                    }
+
+                    else -> {
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(2),
+                            contentPadding = PaddingValues(top = 10.dp, bottom = 80.dp),
+                            horizontalArrangement = Arrangement.spacedBy(16.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp),
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            items(state.recipes) { currentRecipe ->
+                                CustomRecipeCard(
+                                    title = currentRecipe.title,
+                                    hasImage = currentRecipe.hasImage,
+                                    calories = currentRecipe.calories,
+                                    time = currentRecipe.time,
+                                    onEditClick = {}, //
+                                    onDeleteClick = {}, //
+                                )
+                            }
+                        }
+                    }
                 }
             }
 
-            OwnFooter(
-                onAddClick = { viewModel.onAddRecipe()}
-            )
+            if (!state.isFormVisible) {
+                OwnFooter(
+                    onAddClick = { viewModel.onToggleForm(true) }
+                )
+            }
         }
     }
 }
--- composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/own/OwnViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/views/own/OwnViewModel.kt
@@ -3,26 +3,47 @@ package org.example.smartmeal.ui.views.own
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import org.example.smartmeal.ui.utils.Recipe
 
 data class OwnRecipeUIState(
-    val recipes: List<String> = emptyList(),
+    val recipes: List<Recipe> = listOf(
+        Recipe(title = "Grillowany łosoś", calories = "450kcal", time = "25min", hasImage = true),
+        Recipe(title = "Burger wieprzowy", calories = "1500kcal", time = "35min", hasImage = false)
+    ),
     val searchQuery: String = "",
-    val isLoading: Boolean = false
+    val isLoading: Boolean = false,
+    val isFormVisible: Boolean = false
 )
 
 class OwnViewModel: ViewModel() {
 
     private val _uiState = MutableStateFlow(OwnRecipeUIState())
 
+    fun onToggleForm(visible: Boolean) {
+        _uiState.update { it.copy(isFormVisible = visible) }
+    }
     val uiState = _uiState.asStateFlow()
 
     fun onSearchQuery(newQuery: String) {
         _uiState.value = _uiState.value.copy(searchQuery = newQuery)
     }
 
-    fun onAddRecipe() {
-        _uiState.value = _uiState.value.copy(
-            recipes = listOf("Nowy Przepis")
-        )
+    fun onEditRecipe() {} //
+
+    fun onDeleteRecipe() {} //
+
+    fun onAddRecipe(newRecipe: Recipe) {
+
+//        _uiState.value = _uiState.value.copy(
+//            recipes = listOf("Nowy Przepis")
+//        )
+
+        _uiState.update {
+            it.copy(
+                recipes = it.recipes + newRecipe,
+                isFormVisible = false
+            )
+        }
     }
 }

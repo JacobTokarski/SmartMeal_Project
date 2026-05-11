5cd01e5a331b3b523406f916cf5dbb7c74c83c74
Jakub <ktokarski2003@o2.pl>
2026-05-11 15:00:19 +0200

    Created CustomFormField

---
+++ composeApp/src/commonMain/kotlin/org/example/smartmeal/ui/components/CustomFormField.kt
@@ -0,0 +1,30 @@
+package org.example.smartmeal.ui.components
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.example.smartmeal.ui.theme.Colors
+
+@Composable
+fun CustomFormField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        shape = RoundedCornerShape(15.dp),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White,
+            focusedBorderColor = Colors.Primary,
+            unfocusedBorderColor = Colors.Primary,
+        )
+    )
+}
